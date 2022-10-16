1a2f235f432fbf85219477e2240e7923f96af915
KMSAP <kmshero112@gmail.com>
2022-10-16 16:46:45 +0900

    setting flake8

--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -11,7 +11,6 @@ from django.core.management.base import BaseCommand
 
 class Command(BaseCommand):
     """Django command to wait for database."""
-    
     def handle(self, *args, **options):
         """Entrypoint for command."""
         self.stdout.write('Waiting for DB')
@@ -21,7 +20,7 @@ class Command(BaseCommand):
                 self.check(databases=['default'])
                 db_up = True
             except(Psycopg2OpError, OperationalError):
-                self.stdout.write('Database is unavailable, waiting 1 second...')
+                self.stdout.write('Database unavailable, waiting 1 second...')
                 time.sleep(1)
-                
+
         self.stdout.write(self.style.SUCCESS('Database is available!'))
