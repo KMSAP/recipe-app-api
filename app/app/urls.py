2124d078c268a08aa8d9c81a7fb7505b8060b4b2
KMSAP <kmshero112@gmail.com>
2022-10-23 21:02:29 +0900

    Write tests for create user API and create user API

--- app/app/urls.py
+++ app/app/urls.py
@@ -4,7 +4,7 @@ from drf_spectacular.views import (
 )
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -14,4 +14,5 @@ urlpatterns = [
         SpectacularSwaggerView.as_view(url_name='api-schema'),
         name='api-docs',
     ),
+    path('api/user/', include('user.urls')),
 ]
